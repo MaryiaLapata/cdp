5b1843b54a0f3876d18f7f62fafdff42abc8c8a2
Maryia Lapata <Maryia_Lapata@epam.com>
2017-03-15 23:54:47 +0300

    added User dto

---
+++ userManagement/src/main/java/com/epam/cdp/userManagement/UserManagementApp.java
@@ -0,0 +1,14 @@
+package com.epam.cdp.userManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApp.class, args);
+
+	}
+
+}
