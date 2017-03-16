6a9542116088ef01aa0e50ba7be3c43b35831898
Maryia Lapata <Maryia_Lapata@epam.com>
2017-03-16 22:29:13 +0300

    added functionality

--- userManagement/src/main/java/com/epam/cdp/userManagement/UserManagementApp.java
+++ userManagement/src/main/java/com/epam/cdp/userManagement/UserManagementApp.java
@@ -1,14 +1,47 @@
 package com.epam.cdp.userManagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.epam.cdp.userManagement.model.Address;
+import com.epam.cdp.userManagement.model.User;
+import com.epam.cdp.userManagement.util.UserStore;
+
 @SpringBootApplication
 public class UserManagementApp {
+	
+	@Autowired
+	public UserStore userStore;
+
+	
+	public UserManagementApp() {
+        System.out.println("constructor of App");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApp.class, args);
-
+	}
+	
+	@PostConstruct
+	public void init() {
+		List<User> users = new ArrayList<>();
+		User user;
+		Address address;
+		
+		for(int i = 1; i <= 10; i++){
+			address = new Address(i, "Minsk", "Street" + i, i + 10, i*10);
+			user = new User(i*100, "firstName" + i, "lastName" + i, "email" + i + "@dot.com", "1234567", address);
+			
+			users.add(user);
+		}
+		
+		userStore.setUserList(users);
 	}
 
 }
